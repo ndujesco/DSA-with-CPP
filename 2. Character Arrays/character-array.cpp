aa201ce6ee9a81cc4f8174c8fc7dce525a9e4202
ugo <ndujesco@users.noreply.github.com>
2023-11-25 05:35:54 +0100

    add character array

---
+++ 2. Character Arrays/character-array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char name[50];
+
+    cout << "What is your name? ";
+    cin >> name;
+    cout << "Your name is " << name << endl;
+
+    return 0;
+}
---
+++ 2. Character Arrays/length-of-string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int length(char input[])
+{
+    int count = 0;
+    char value = input[count];
+
+    while (value != '\0')
+    {
+        count++;
+        value = input[count];
+    }
+
+    return count;
+}
+
+int main()
+{
+    char name[100];
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "The length of characters in your name is " << length(name) << endl;
+
+    return 0;
+}
