2b4b03df73a67eaca6ae77865ba83e536e4b670b
ugo <ndujesco@users.noreply.github.com>
2023-11-26 17:27:26 +0100

    update from 2

--- 2. Character Arrays/inbuilt-functions.cpp
+++ 2. Character Arrays/inbuilt-functions.cpp
@@ -4,20 +4,49 @@
 using namespace std;
 
 /*
-strlen(value)  finds the length up to the first space.
-strcmp(value1, value2)  returns 0 if they match and some other integer (-1 OR 1), otherwise.
-strcpy(dest_string, source_string)  
+strlen(string)                          finds the length up to the first space.
+strcmp(string1, string2)                returns 0 if they match and some other integer (-1 OR 1), otherwise.
+strcpy(dest_string, source_string)      after changing each character it inputs the null character to indicate the end.
+strncpy(dest_string, source_string, n)  copies the first n characters from the source_string
+strcat(string1, string2)                concactenates string2 after string1
 */
+
+int mystrlen(char input[])
+{
+    int count = 0;
+    char value = input[count];
+
+    while (value != '\0')
+    {
+        count++;
+        value = input[count];
+    }
+
+    return count;
+}
+
+bool mystrcmp(char str1[], char str2[])
+{
+    if (mystrlen(str1) != mystrlen(str2))
+        return false;
+
+    for (int i = 0; str1[i] != '\0'; i++)
+    {
+        if (str1[i] != str2[i])
+            return false;
+    }
+
+    return true;
+}
 
 int main()
 {
+    char str1[] = "abeddd";
+    char str2[] = "abc";
 
-    char str1[100];
-    cin >> str1;
-    int len = strlen(str1);
+    // cout << mystrcmp(str1, str2) << endl; 
+    strncpy(str1, str2, 4);
+    cout << str1 << endl;
 
-    
-
-    cout << strcmp("zba", "azc") << endl; // 
     return 0;
 }
